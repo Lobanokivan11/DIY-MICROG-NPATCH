d5c1be914f20cb2efe5e4e60ba33342b76f8bcb1
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-08 15:33:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,9 @@ git lfs install
 git submodule update --init --recursive
 git clone https://github.com/microg/GmsCore.git input
 cd input
-find . -name "build.gradle" -o -name "build.gradle.kts" | xargs sed -i '/defaultConfig {/a \        applicationId "top.nkbe.npatch.gms"' || true
+sed -i '/android.applicationVariants.all { variant ->/a \    variant.mergedFlavor.applicationId = "top.nkbe.npatch.gms"' play-services-core/build.gradle
 find . -name "build.gradle" -o -name "build.gradle.kts" | xargs sed -i 's/"package_id", "com\.google\.android\.gms"/"package_id", "top.nkbe.npatch.gms"/g' || true
 find . -name "build.gradle" -o -name "build.gradle.kts" | xargs sed -i 's/"package_id", "org\.microg\.gms"/"package_id", "top.nkbe.npatch.gms"/g' || true
-find . -name "build.gradle" -o -name "build.gradle.kts" | xargs sed -i 's/applicationId = "org\.microg\.gms"/applicationId = "top.nkbe.npatch.gms"/g' || true
 find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.gms/top.nkbe.npatch.gms/g' || true
 cp -r ../profiles/*.xml play-services-core/src/main/res/xml
 export GRADLE_MICROG_VERSION_WITHOUT_GIT=0
