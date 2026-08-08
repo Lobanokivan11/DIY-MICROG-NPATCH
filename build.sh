11a437f21d26e85946004e1b3975567fefb75749
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-08 16:23:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,18 @@ find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.googl
 find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.gtalkservice/top.nkbe.npatch.gtalkservice/g' || true
 find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.providers\.gsf/top.nkbe.npatch.gsf/g' || true
 find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.c2dm/top.nkbe.npatch.c2dm/g' || true
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.gms/top.nkbe.npatch.gms/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.googleapps/top.nkbe.npatch.googleapps/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.gtalkservice/top.nkbe.npatch.gtalkservice/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.providers\.gsf/top.nkbe.npatch.gsf/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.c2dm/top.nkbe.npatch.c2dm/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.finsky/top.nkbe.npatch.finsky/g' {} +
+find . -type f -name "*.xml" -exec sed -i 's/com\.google\.android\.apps\.maps/top.nkbe.npatch.maps/g' {} +
+find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.gms/top.nkbe.npatch.gms/g' || true
+find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.googleapps/top.nkbe.npatch.googleapps/g' || true
+find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.gtalkservice/top.nkbe.npatch.gtalkservice/g' || true
+find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.providers\.gsf/top.nkbe.npatch.gsf/g' || true
+find . -name "AndroidManifest.xml" | xargs sed -i 's/com\.google\.android\.c2dm/top.nkbe.npatch.c2dm/g' || true
 cp -r ../profiles/*.xml play-services-core/src/main/res/xml
 export GRADLE_MICROG_VERSION_WITHOUT_GIT=0
 ./gradlew :play-services-core:assembleMapboxDefault
