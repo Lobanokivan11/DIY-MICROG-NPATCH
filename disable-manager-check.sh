f4d82792f8e65e7f0a117c0f40cc88245e4c4728
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-08 19:50:33 +0300

    Update disable-manager-check.sh

--- disable-manager-check.sh
+++ disable-manager-check.sh
@@ -51,7 +51,7 @@ if [ ! -f "$SMALI_FILE" ]; then
 fi
 
 # Code modification: inject 'const/4 v5, 0x1' before the conditional jump
-sed -i '/[[:space:]]*if-nez v5, :cond_f/i \    const/4 v5, 0x1' "$SMALI_FILE"
+patch "$SMALI_FILE" < disable-check-manager.patch
 
 # Assemble the modified smali files back into the original dex file structure
 smali a tmp_smali -o "tmp_dex/$FOUND_DEX"
