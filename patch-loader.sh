c27e559a8e4ac2ec8a19f345c9fc17d5e50f0a28
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-08 19:11:26 +0300

    Update patch-loader.sh

--- patch-loader.sh
+++ patch-loader.sh
@@ -1,6 +1,3 @@
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install libsmali-java -y
 baksmali d loader.bin -o out_folder
 find out_folder/ -type f -exec sed -i 's/app\.revanced\.android\.gms/top\.nkbe\.npatch\.gms/g' {} +
 smali a out_folder -o new_loader.bin
