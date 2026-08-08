295e4018a54735e801661deaae3ddc441de95f81
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-08 14:17:42 +0300

    Create patch-loader.sh

---
+++ patch-loader.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install libsmali-java -y
+baksmali d loader.bin -o out_folder
+find out_folder/ -type f -exec sed -i 's/app\.revanced\.android\.gms/top\.nkbe\.npatch\.gms/g' {} +
+smali a out_folder -o new_loader.bin
+echo "there is new_loader.bin file rename to loader.bin and place inside npatch's manager apk or cli tool jar sign and install"
